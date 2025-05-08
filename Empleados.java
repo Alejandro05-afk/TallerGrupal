ee1282b3a3a54b7ef8d4d062d3aec821ed439dfb
POO <xavier.guanoluisa@epn.edu.ec>
2025-05-08 09:30:39 -0500

    GuanoluisaAlejandro (ingresar nombre del empleado)

---
+++ Empleados.java
@@ -0,0 +1,15 @@
+public class Empleados {
+    int salario;
+    int edad;
+    String nombre;
+    public Empleados(int salario, int edad, String nombre) {
+        this.salario = salario;
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+    public String nombre() {
+        return nombre;
+    }
+
+
+}
