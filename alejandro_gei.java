470d29500f8264ab6ba70951e8b5441edf74b790
POO5 <santiago.vargas@epn.edu.ec>
2025-05-08 09:22:04 -0500

    project

--- alejandro_gei.java
+++ alejandro_gei.java
@@ -1,2 +1,10 @@
 public class alejandro_gei {
+    int salario;
+    int edad;
+    String nombre;
+    public alejandro_gei(int salario, int edad, String nombre) {
+        this.salario = salario;
+        this.edad = edad;
+    }
+
 }
