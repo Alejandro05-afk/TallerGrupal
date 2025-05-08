2d4a2b9d07301a8f484629a46508c493ae511bbe
POO <eduardo.ganchala@eon.edu.ec>
2025-05-08 09:46:36 -0500

    Maria Giron: Se realizó la creación del método para validar que el salario del empleado sea positivo.

--- Empleados.java
+++ Empleados.java
@@ -2,11 +2,13 @@ public class Empleados {
     private int salario;
     private int edad;
     private String nombre;
+
     public Empleados(int salario, int edad, String nombre) {
         this.salario = salario;
         this.edad = edad;
         this.nombre = nombre;
     }
+
     public String nombre() {
         return nombre;
     }
@@ -19,15 +21,27 @@ public class Empleados {
         this.nombre = nombre;
 
     }
+
     public int getSalario() {
         return salario;
     }
+
     public void setSalario(int salario) {
         this.salario = salario;
     }
+
     public int getEdad() {
         return edad;
     }
 
-
+    // Metodo para validar que el salario del empleado sea positivo
+    public double validarSalarioEmpleado() {
+        if (salario >= 0) {
+            System.out.println("El salario es correcto, puede proceder con el registro del empleado");
+        } else
+            System.out.println("El salario debe ser mayor que 0");
+        return 0;
+    }
 }
+
+
